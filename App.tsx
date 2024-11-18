9a486881dfe38d283ff148408a2b64c1e4268aaf
Bela Bohlender <bela@bohlender.email>
2024-11-17 19:56:35 -0500

    triplex demo version

--- App.tsx
+++ App.tsx
@@ -1,38 +1,52 @@
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas, useFrame, Vector3 } from "@react-three/fiber";
 import { useState, useRef } from "react";
 import { useGLTF, Environment } from "@react-three/drei";
 import { createXRStore, XR } from "@react-three/xr";
+import { Mesh } from "three";
 
 const store = createXRStore({ emulate: { inject: true } });
 
 export default function App() {
-return (<>
-  <button style={{ position: "absolute", zIndex: "1" }} onClick={() => store.enterVR()}>
-      Enter VR
-  </button>
-  <Canvas style={{ position: "absolute", inset: "0", touchAction: "none" }}>
-    <XR store={store}>
-      <Environment preset="warehouse" />
+  return (
+    <>
+      <button
+        style={{ position: "absolute", zIndex: "1" }}
+        onClick={() => store.enterVR()}
+      >
+        Enter VR
+      </button>
+      <Canvas style={{ position: "absolute", inset: "0", touchAction: "none" }}>
+        <XR store={store}></XR>
+      </Canvas>
+    </>
+  );
+}
+
+export function Scene() {
+  return (
+    <>
+      <pointLight color="red" position={[-1, 0, 0]} />
+      <pointLight color="green" position={[1, 0, 0]} />
+      <pointLight color="blue" position={[0, 0, 1]} />
       <MyComponent />
       <MyModel />
-    </XR>
-  </Canvas>
-  </>
-)
+    </>
+  );
 }
 
-function MyModel() {
-  const { scene } = useGLTF("https://www.immersive-react.com/shoe.glb")
-  return <primitive object={scene} />
+function MyModel({ position }: { position?: Vector3 }) {
+  const { scene } = useGLTF("https://www.immersive-react.com/shoe.glb");
+  return <primitive position={position} object={scene} />;
 }
 
-function MyComponent() {
-  const [red, setRed] = useState(true)
-  const ref = useRef<Mesh>(null)
-  useFrame(() => ref.current?.rotateY(0.01)) 
-  return <mesh ref={ref} onClick={() => setRed(!red)} position={[ 0, 0, 0 ]}>
-      <boxGeometry/>
-      <meshBasicMaterial color={red ? "red" : "blue"} />
-  </mesh>
+function MyComponent({ position }: { position?: Vector3 }) {
+  const [red, setRed] = useState(true);
+  const ref = useRef<Mesh>(null);
+  useFrame(() => ref.current?.rotateY(0.01));
+  return (
+    <mesh position={position} ref={ref} onClick={() => setRed(!red)}>
+      <boxGeometry />
+      <meshStandardMaterial color={red ? 0xffffff : 0x999999} />
+    </mesh>
+  );
 }
-
